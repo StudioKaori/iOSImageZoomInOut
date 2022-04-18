0fdaeac2389f150713aed956a808a885d8258f87
Studio Kaori <studiokaori.com@gmail.com>
2022-04-18 21:48:29 +0200

    Update: viewController the UIImage to pinch in/out

--- ImageZoomInOut/ViewController.swift
+++ ImageZoomInOut/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let image = UIImage(named: "image1")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.image = image
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 4.0
     }
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        print("view for zooming")
+        return imageView
+    }
+    
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        print("Zoom start")
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        print("Zoom end")
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        print("Did zoom")
+    }
 
 }
 
